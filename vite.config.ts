cda77aa7353d590b256c9a254d9924f391168b02
EverAfterInvitesCo <contact.everafterinvites@gmail.com>
2026-08-28 03:42:47 +0300

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: '/Zainab-and-Hasan/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
